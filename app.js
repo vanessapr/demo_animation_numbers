9a891446869c2a20f4846cebbba824ccf50da758
Vanessa Milagros <vanessapr.85@gmail.com>
2016-05-03 09:21:49 -0500

    added function for to render numbers

--- app.js
+++ app.js
@@ -4,7 +4,19 @@ $(function() {
     var txtNumbers = event.target.numbers.value;
     var numbers = txtNumbers.split(',');
     if(numbers) {
-      console.log('numbers', numbers)
+      var htmlNumber = renderNumbers(numbers);
+      $('#result').empty().html(htmlNumber);
     }
   });
+
+  function renderNumbers(numbers) {
+    var count = numbers.length;
+    var result = `<ul style="width:${70*count};padding-top:120px;overflow-x:auto;">`;
+    numbers.forEach(function(item, index) {
+      result+=`<li class="block nro-${item}">${item}</li>`;
+    });
+    result+='</ul>';
+    return result;
+  }
+
 });
